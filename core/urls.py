9763e9c48adfb854935e3f6c454198e8cf8c35f4
Roosc Zaño <zanoroosc@gmail.com>
2026-06-10 17:10:17 +0800

    Delete error project app directory

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.note_list ,name="notes"),
-    path('create/', views.create, name="create"),
-    path('edit/<int:id>/', views.edit, name="edit"),
-    path('delete/<int:id>/', views.delete, name="delete"),
-]
