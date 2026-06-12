041eca1c6657f1352bdc5b13d3f29abe588fb3f7
ur1el0 <zanoroosc@gmail.com>
2026-06-12 08:37:41 +0800

    feat(mobile data): added mock data | fix dependency errors

--- mobile/app/src/main/java/com/example/mobile/MainActivity.kt
+++ mobile/app/src/main/java/com/example/mobile/MainActivity.kt
@@ -19,29 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MobileTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+//                AppNavigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MobileTheme {
-        Greeting("Android")
     }
 }
